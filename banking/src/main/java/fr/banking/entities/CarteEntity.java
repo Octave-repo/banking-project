cc49624d9e741f5ac5823ec50f3c18b648e1ad8a
Octave-portfolio <valadieroctave@gmail.com>
2022-11-22 12:05:09 +0100

    Modification CarteEntity

--- banking/src/main/java/fr/banking/entities/CarteEntity.java
+++ banking/src/main/java/fr/banking/entities/CarteEntity.java
@@ -4,8 +4,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class CarteEntity {
+    @Id
+    private String cardNumber;
+
+    @ManyToOne
+    private CompteEntity compte;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String moisExpiration;
+    @NotBlank
+    private String anneeExpiration;
+
 }
