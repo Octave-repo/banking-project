2b5910b0c1e4d5279017f4c2cec99463b5215df6
Octave-portfolio <valadieroctave@gmail.com>
2022-11-22 12:05:33 +0100

    Modification ClientEntity

--- banking/src/main/java/fr/banking/entities/ClientEntity.java
+++ banking/src/main/java/fr/banking/entities/ClientEntity.java
@@ -4,19 +4,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class ClientEntity {
     @Id
     @GeneratedValue
     private long id;
+    @NotBlank
     private String nom;
+    @NotBlank
     private String prenom;
-    private int age;
+    @NotBlank
+    private Date dateNaissance;
+    @NotBlank
     private String adresse;
+    @NotBlank
     private String telephone;
+    @ManyToMany
+    private List<CompteEntity> comptes;
+    @NotBlank
+    private Date dateCreation;
+    private Date dateModification;
 }
