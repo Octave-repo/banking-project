227e2f77e0754e7d16ef0fb1afc6f7b599b59012
Octave-portfolio <valadieroctave@gmail.com>
2022-11-22 12:05:56 +0100

    Modification CompteEntity

--- banking/src/main/java/fr/banking/entities/CompteEntity.java
+++ banking/src/main/java/fr/banking/entities/CompteEntity.java
@@ -4,13 +4,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class CompteEntity {
     @Id
     private String iBAN;
-    private Double solde;
+    @NotBlank
+    private double solde;
+    private String intituleCompte;
+    private TypeCompte typeCompte;
+
+    @ManyToMany
+    List<ClientEntity> clients;
+
+    @OneToMany
+    List<CarteEntity> cartes;
+    @NotBlank
+    private Date dateCreation;
 }
