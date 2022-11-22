b4dd6b99a63d88a927c903a9f1987ef6af8cd7dc
Octave-portfolio <valadieroctave@gmail.com>
2022-11-22 11:03:06 +0100

    Ajout CarteEntity

--- banking/src/main/java/fr/banking/entities/CarteEntity.java
+++ banking/src/main/java/fr/banking/entities/CarteEntity.java
@@ -4,13 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Id;
-
 @Getter
 @Setter
 @NoArgsConstructor
-public class CompteEntity {
-    @Id
-    private String iBAN;
-    private Double solde;
+public class CarteEntity {
 }
